a1e300c4cbacbf47b2d12dbaee2280b6ed9a606d
valdano <valdanoesnadiar@gmail.com>
2024-01-31 03:44:47 +0700

    asdjaosjdojas

--- react-tutorial-praktek/src/App.js
+++ react-tutorial-praktek/src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
+import Mobil from './Component/Mobil'
 
 
-const ProductCard = () => {
-  return (
-    <div className="card">
-      <img style={{width: "100%", height: "200px", borderRadius: "10px 10px 0 0"}} src="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" alt="" />
 
-      <div className="container">
-          <h4>
-            <b>Ford</b>
-          </h4>
-          <p>Mobil Ford adalah ....</p>
-      </div>
+// const ProductCard = () => {
+//   return (
+//     <div className="card">
+//       <img style={{width: "100%", height: "200px", borderRadius: "10px 10px 0 0"}} src="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" alt="" />
 
-    </div>
+//       <div className="container">
+//           <h4>
+//             <b>Ford</b>
+//           </h4>
+//           <p>Mobil Ford adalah ....</p>
+//       </div>
 
-  )
-}
+//     </div>
+
+//   )
+// }
 
 function App() {
   return (
     <div className="cards">
-      <ProductCard />
+        <Mobil gambar="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" tipe="Ford"  deskripsi="Mobil ford adalah mobil yang sangat bagus" />
+        <Mobil gambar="https://images.unsplash.com/photo-1584345604476-8ec5e12e42dd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTMyMzE1OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" tipe="Mustang"  deskripsi="Mobil Mustang mempunyai desain yang elegan" />
+        <Mobil gambar="https://images.unsplash.com/photo-1559416523-140ddc3d238c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5MDE0MjIxNg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" tipe="Toyota"  deskripsi="Mobil toyota memiliki interior dan body yang kokoh" />
     </div>
   );
 }
---
+++ react-tutorial-praktek/src/Component/Mobil.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Mobil = (props) => {
+    return(
+        <div className="card">
+            <img style={{width: "100%", height: "200px", borderRadius: "10px 10px 0 0"}} src={props.gambar} alt="" />
+            <div className="container">
+                <h4><b>{props.tipe}</b></h4>
+            </div>
+            <p>{props.deskripsi}</p>
+        </div>
+        
+    )
+}
+
+export default Mobil
