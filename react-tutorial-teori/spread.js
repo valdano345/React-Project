87d62550213974e7b2ac5e446a9c4117ab8f951c
valdano <valdanoesnadiar@gmail.com>
2024-02-02 00:19:14 +0700

    sdodoaods

---
+++ react-tutorial-teori/spread.js
@@ -0,0 +1,18 @@
+// const myProfile = {
+//     nama: "Ranggo",
+//     hobi: "Basket",
+//     agama: "islam"
+// }
+
+// const{nama, hobi, agama} = myProfile
+// console.log(nama, hobi, agama);
+
+// const updateProfile = {
+//     nama: "Valdano",
+//     "Makanan Favorit": "telor"
+// }
+
+// const myUpdateProfile = { ...updateProfile, ...myProfile}
+// console.log(myUpdateProfile);
+
+
--- react-tutorial-teori/src/App.js
+++ react-tutorial-teori/src/App.js
@@ -13,17 +13,27 @@ const App = () => {
         setCounter(counter + 1)
     }
 
-    const [nama, setNama] = useState('');
+    // const [nama, setNama] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault(); //sebelum logic atau initiate di onSubmit harus e.preventDefault agar tidak tereload page nya
-        console.log(nama);
+        console.log(data);
+        setData(initialState);
 
     }
+    
+    const initialState = {
+        nama: "",
+        hobi: "",
+        agama: ""
+    }
+    const [data, setData] = useState(initialState);
+    const {nama, hobi, agama} = data
+
     const handleChange = (e) =>{
-        setNama(e.target.value)
+        setData({...data, [e.target.name] : e.target.value})
         // console.log(nama);
     }
-
+    
     return(
         <div>
             {/* <button onClick={decrementCounter}>
@@ -37,7 +47,12 @@ const App = () => {
 
             <form onSubmit={handleSubmit} action="">
                 <label htmlFor="">Nama</label>
-                <input type="text" onChange={handleChange} value={nama} />
+                <input type="text" onChange={handleChange} value={nama} name="nama" />
+                <label htmlFor="">Hobi</label>
+                <input type="text" onChange={handleChange} value={hobi} name="hobi" />
+                <label htmlFor="">Agama</label>
+                <input type="text" onChange={handleChange} value={agama} name="agama" />
+                <input type="submit" value="Kirim" />
             </form>
         </div>
     )
