02a26d778e113ac61cf1e3e768414418f51a60f3
valdano345 <valdanoesnaidar@gmail.com>
2024-02-26 11:13:11 +0700

    asdnjasjidn

---
+++ kc-react-router-v6-main/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navigation from './Components/Navigation';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Cars from './Pages/Cars';
+import CarDetail from './Pages/CarDetail';
+import SecondCar from './Pages/SecondCar';
+import Contact from './Pages/Contact';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ kc-react-router-v6-main/src/Components/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/cars">Cars</a>
+                </li>
+                <li>
+                    <a href="/Contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/">About</a>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default Navigation
