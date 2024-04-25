a7542cd27bf2bb148b08330d42f091a4ec2bd4d9
valdano <valdanoesnadiar@gmail.com>
2024-04-26 05:29:14 +0700

    clear middleware

--- my-app/src/pages/product/index.tsx
+++ my-app/src/pages/product/index.tsx
@@ -2,39 +2,27 @@ import { fetcher } from "@/lib/firebase/swr/fetcher";
 import ProductView from "@/views/Product";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import useSWR from 'swr'
+import useSWR from "swr";
 
+const ProductPage = () => {
+  const [products, setPorducts] = useState([]);
+  const { push } = useRouter();
 
+  const { data, error, isLoading } = useSWR("/api/products", fetcher);
 
+  // useEffect(() => {
+  //     fetch('/api/products')
+  //         .then((res) => res.json())
+  //         .then((response) => {
+  //             setPorducts(response.data);
+  //         })
+  // }, [])
 
-const ProductPage = () =>{
-    const [isLogin, setIsLogin] = useState(true);
-    // const [products, setPorducts] = useState([]);
-    const {push} = useRouter();
-    useEffect(() => {
-        if(!isLogin){
-            push('/auth/login')
-        }
-    }, [])
-
-    const {data, error, isLoading} = useSWR(
-        "/api/products",fetcher
-    )
-
-    // useEffect(() => {
-    //     fetch('/api/products')
-    //         .then((res) => res.json())
-    //         .then((response) => {
-    //             setPorducts(response.data);
-    //         })
-    // }, [])
-
-
-    return(
-        <div>
-            <ProductView products={isLoading ? [] : data.data} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <ProductView products={isLoading ? [] : data.data} />
+    </div>
+  );
+};
 
 export default ProductPage;
--- my-app/src/views/Auth/Login/index.tsx
+++ my-app/src/views/Auth/Login/index.tsx
@@ -1,22 +1,24 @@
-import Link from "next/link"
-import { useRouter } from "next/router"
-import style from './Login.module.scss'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import style from "./Login.module.scss";
 
-const LoginView = () =>{
-    const {push} = useRouter();
-    const handleLogin = () => {
-        push('/product')
-    }
+const LoginView = () => {
+  const { push } = useRouter();
+  const handleLogin = () => {
+    push("/product");
+  };
 
-    return(
-        <div className={style.login}>
-            <h1 className="text-3xl">Login Page</h1>
-            <button onClick={handleLogin}>Login</button>
-            <p style={{color: "red", border: "1px solid red", borderRadius: "10px"}}>
-                Belum punya akun? registrasi <Link href={"/auth/register"}>disini</Link>
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className={style.login}>
+      <h1 className="text-3xl">Login Page</h1>
+      <button onClick={handleLogin}>Login</button>
+      <p
+        style={{ color: "red", border: "1px solid red", borderRadius: "10px" }}
+      >
+        Belum punya akun? registrasi <Link href={"/auth/register"}>disini</Link>
+      </p>
+    </div>
+  );
+};
 
-export default LoginView
+export default LoginView;
