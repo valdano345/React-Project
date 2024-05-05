86a4a8751fe3aa02aec6aa00d3ba2128f6b5d94c
valdano345 <valdanoesnaidar@gmail.com>
2024-05-05 15:30:35 +0700

    asdsda

--- my-app/src/components/layouts/Navbar/index.tsx
+++ my-app/src/components/layouts/Navbar/index.tsx
@@ -1,12 +1,25 @@
-import styles from "./Navabr.module.css"
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "./Navabr.module.css";
 
-const Navbar = () =>{
-    
-    return(
-        <div className={styles.navbar}>
-            <div className="big">Navbar</div>
-        </div>
-    )
-}
+const Navbar = () => {
+  const { data }: any = useSession();
+  return (
+    <div className={styles.navbar}>
+      <div className="big">Navbar</div>
+      <div>
+        {data && data.user.fullname}{" "}
+        {data ? (
+          <button className={styles.button} onClick={() => signOut()}>
+            Sign Out
+          </button>
+        ) : (
+          <button className={styles.button} onClick={() => signIn()}>
+            Sign In
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
